b6f5d4db6a795d79d72623c4d3432e3ca8a2d235
hacivelikarakus <karakus.haciveli@gmail.com>
2024-10-15 09:41:47 +0300

    fix main activity

--- app/src/main/java/com/hvk/murgichat/MainActivity.kt
+++ app/src/main/java/com/hvk/murgichat/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MurgiChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "hello",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
